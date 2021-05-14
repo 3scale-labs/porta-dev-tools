a966350456b1ae06a75baa9e63e03e9b69e222b6
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-05-14 22:21:06 +0300

    hosts entries on container startup

--- porxy/docker-entrypoint.sh
+++ porxy/docker-entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-ip=$(ip -4 addr show docker0 2>/dev/null | grep -Po 'inet \K[\d.]+')
-if [ -z "$ip" ];then
-  ip=$(ping -c 1 host.docker.internal | head -n 1 | sed -r 's/PING\shost\.docker\.internal\s\(([^\)]+).*/\1/')
-fi
-
-echo -e "${ip}\tmaster-account.3scale.localhost">>/etc/hosts
-
 exec "$@"
